eadf7c519ae7225d2603bef8c6b502ed7a60d6d4
Angelo Henrique Almeida <85762595+Angelohenrique9@users.noreply.github.com>
2021-11-30 16:21:54 -0300

    Create navbar.php

---
+++ includes/navbar.php
@@ -0,0 +1,14 @@
+
+<!--Barra superior de navegação da página-->
+
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+    <a class="navbar-brand" href="#">
+        <img src="image/softlaw.png" alt="Logo" style="height:32px;"> 
+    </a>
+    <li class="nav-item">
+        <a class="nav-link" href="lista.php">Minhas Audiências</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="cadastro.php">Criar Audiências</a>
+    </li>
+</nav>
